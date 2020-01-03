261058a0f045e2373ec7a8019c4b0f3e04344b14
kbub27 <kalebmerriman27@gmail.com>
2020-01-03 13:57:30 -0800

    made portfolio link to usable portoflio

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h1>Current portfolio is being rebuilt</h1>
+      <h2>Link below will reroute to other portfolio</h2>
       
+      <a 
+        href="https://kbub27.github.io/Portfolio"
+        className="App-link"
+        target="_blank"
+        rel="noopener noreferrer">
+        CLICK HERE
+      </a>
     </div>
   );
 }
